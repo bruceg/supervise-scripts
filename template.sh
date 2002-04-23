76f73a3591115e5c68641149d0859874053275c4
Bruce Guenter <bruce@untroubled.org>
2002-04-23 02:42:11 +0000

    Make both configure use @VAR@ for both template and the scripts.

--- template.sh
+++ template.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
+# WARNING: This file was auto-generated. Do not edit!
 set -e
 PATH="/bin:/usr/bin:/usr/local/bin:$PATH"
 export PATH
 cd "${SVSCANDIR-/service}"
 
 fatal() {
-  echo "%PROGRAM%: Fatal error: $@" >&2
+  echo "@PROGRAM@: Fatal error: $@" >&2
   exit 1
 }
 
 usage() {
   args=$1; min=$2; max=$3; shift 3
   if [ $args -lt $min -o $args -gt $max ]; then
-    echo "$0: usage: %PROGRAM% $@" >&2
+    echo "$0: usage: @PROGRAM@ $@" >&2
     exit 1
   fi
 }
