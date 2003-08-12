3e34470b1b97d264da992aa6ce0dc63445923f13
Bruce Guenter <bruce@untroubled.org>
2003-08-12 18:23:16 +0000

    Create a usage_nocd function that does the usage bit, but doesn't chdir into /service.

--- template.sh
+++ template.sh
@@ -2,18 +2,22 @@ set -e
 PATH="/bin:/usr/bin:/usr/local/bin:$PATH"
 export PATH
 program=`basename $0`
-cd "${SVSCANDIR-/service}"
 
 fatal() {
   echo "$program: Fatal error: $@" >&2
   exit 1
 }
 
-usage() {
+usage_nocd() {
   args=$1; min=$2; max=$3; shift 3
   if [ $args -lt $min -o $args -gt $max ]; then
     echo "$0: usage: $program $@" >&2
     exit 1
   fi
+}
+
+usage() {
+  usage_nocd "$@"
+  cd "${SVSCANDIR-/service}"
 }
 
